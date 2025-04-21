9e379e75aed555fef19c543c374a12a2154a8d7f
Akshita2020 <akshita.tiwari2020@gmail.com>
2025-04-21 13:18:42 +0530

    Social Media app Setup and added google custom fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {View, Text} from 'react-native';
+import {getFontFamily} from './assets/fonts/helper';
 
 function App() {
   return (
     <View>
-      <Text style={{fontSize: 50, fontFamily: 'Inter'}}>
-        Hello, World! Welcome to our social media app
+      <Text
+        style={{fontSize: 50, fontFamily: getFontFamily('Inter_18pt', '100')}}>
+        "Hello, World! Welcome to our social media app"
       </Text>
     </View>
   );
---
+++ assets/fonts/helper.ts
@@ -0,0 +1,28 @@
+// to make sure that fonts look similar to diffrent operation systems
+
+export const getFontFamily = (baseFont: string = 'Inter', weight: any) => {
+  switch (weight) {
+    case '100':
+      return `${baseFont}-Thin`;
+    case '200':
+      return `${baseFont}-ExtraLight`;
+    case '300':
+      return `${baseFont}-Light`;
+    case 'normal':
+    case '400':
+      return `${baseFont}-Regular`;
+    case '500':
+      return `${baseFont}-Medium`;
+    case '600':
+      return `${baseFont}-SemiBold`;
+    case 'bold':
+    case '700':
+      return `${baseFont}-Bold`;
+    case '800':
+      return `${baseFont}-ExtraBold`;
+    case '900':
+      return `${baseFont}-Black`;
+    default:
+      return `${baseFont}-Regular`;
+  }
+};
